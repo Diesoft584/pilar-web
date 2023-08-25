bee6831350e0c6157663d775f9ea52e3591c2f61
Diego <oviedodiego584@gmail.com>
2023-08-25 00:24:04 -0300

    React_tp3: Trabajo Práctico 3 - Completado

---
+++ src/constants/index.js
@@ -0,0 +1,5 @@
+export const BASE_URL = "https://pokeapi.co/api/v2";
+export const IMG_URL =
+  "https://assets.pokemon.com/assets/cms2/img/pokedex/full/";
+
+//6° Sexto de la guia 3 Importar las librerias de los pokemos con sus imagens
